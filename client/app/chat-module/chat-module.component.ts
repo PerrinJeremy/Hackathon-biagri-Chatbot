db6c51a77697b2bc77a01d26b75f4b2d46e06d9e
SYLVAIN LE CROM <sylvain.lecrom@free.fr>
2018-01-11 15:03:09 +0100

    set webhook

--- client/app/chat-module/chat-module.component.ts
+++ client/app/chat-module/chat-module.component.ts
@@ -1,3 +1,5 @@
+import { IMessage } from './../shared/models/message.model';
+import { client } from './../dialog-flow-client/dialog-flow.client';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';  
 import { ChatService, Message } from '../services/chat.service';  
 import { Observable } from 'rxjs/Observable';  
@@ -12,13 +14,33 @@ export class ChatModuleComponent implements OnInit {
   messages: Observable<Message[]>;  
   strMsg: string;  
   constructor(private chat: ChatService) { }
+  conversation: IMessage[] = [];
 
   ngOnInit() {
     this.messages = this.chat.conversation.asObservable()  
-    .scan((acc, val) => acc.concat(val)); 
+    .scan((acc, val) => acc.concat(val));
   }
   sendMessage() {  
-    this.chat.talk(this.strMsg);  
-    this.strMsg = '';  
+    this.chat.talk(this.strMsg); 
+    this.strMsg = '';
   }
+
+  addMessageFromUser(message) {
+    this.conversation.push({
+      avatar: 'perm_identity',
+      from: 'Me',
+      content: message.value
+    });
+
+    client.textRequest(message.value).then((response) => {
+      this.conversation.push({
+        avatar: 'android',
+        from: 'Bot',
+        content: response.result.fulfillment['speech'] || 'I can\'t seem to figure that out!'
+      });
+      message.value = '';
+    });
+  }
+
+
 }
---
+++ client/app/dialog-flow-client/dialog-flow.client.ts
@@ -0,0 +1,4 @@
+import { ApiAiClient } from "api-ai-javascript";
+
+// You'll get this access token from Your Agent Dashboard > Settings > General > API keys > Developer access token
+export const client = new ApiAiClient({accessToken: '8f77ab0f230d4ce19701f56ea55895b9'});
